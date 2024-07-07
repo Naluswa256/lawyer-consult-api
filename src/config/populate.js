fd90a27115c4a6813adc2b794859a98f3f421c13
williamnaluswa <williamnaluswa@gmail.com>
2024-07-07 15:37:07 +0000

    fix another populate

--- src/config/populate.js
+++ src/config/populate.js
@@ -71,7 +71,7 @@ const specializations = [
   const sampleLawyers = [
   
     {
-      email: 'lawyer27@example.com',
+      email: 'lawyer28@example.com',
       fullNames: 'Kakembo charles',
       phoneNumber: '1234567890',
       location: 'Kampala, Uganda',
